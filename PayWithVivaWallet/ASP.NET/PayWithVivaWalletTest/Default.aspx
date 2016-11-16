c38d5d1f6c26feeed2773f741ecf467c28af7ef2
philosopher <philosopher@vivawallet.com>
2016-11-16 14:29:30 +0200

    Added PayWithVivaWallet sample

---
+++ PayWithVivaWallet/ASP.NET/PayWithVivaWalletTest/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="Home Page" Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication2._Default" Async="true" %>
+
+<html>
+    <body>
+        <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+
+
+<script src="https://demo.vivapayments.com/web/checkout/js"></script>
+<form id="myform" method="post">
+<button type="button"
+data-vp-sourcecode="Default"
+data-vp-publickey="Ob/Mdq9m9azAOHhsX1m8FHOYYaa95hY+mPAqGuJAyAQ="
+data-vp-baseurl="https://demo.vivapayments.com"
+data-vp-lang="el"
+data-vp-amount="1000"
+data-vp-customeremail="customer@vivawallet.com"
+data-vp-customerfirstname = "John"
+data-vp-customersurname = "Smith"
+data-vp-merchantref="test merchant ref"
+data-vp-expandcard="true"
+data-vp-description="My product">
+</button>
+</form>
+
+    </body>
+</html>
---
+++ PayWithVivaWallet/ASP.NET/PayWithVivaWalletTest/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RestSharp;
+
+namespace WebApplication2
+{
+    public partial class _Default : Page
+    {
+        private const string merchantId = "6466348D-85B2-4CBC-978B-422C688D2D45";
+        private const string apiKey = "Y^!xL#";
+
+        protected async void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.HttpMethod == "POST") {
+                var cl = new RestClient("https://demo.vivapayments.com/api/") {
+                    Authenticator = new HttpBasicAuthenticator(merchantId, apiKey)
+                };
+                var request = new RestRequest("transactions", Method.POST) {
+                    RequestFormat = DataFormat.Json
+                };
+
+                request.AddParameter("PaymentToken", Request.Form["vivaWalletToken"]);
+
+                var response = await cl.ExecuteTaskAsync<TransactionResult>(request);
+
+                if (response.Data != null) {
+                    Response.Write(response.Data.ErrorCode + "--" + response.Data.ErrorText);
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                        Response.Write("<br />Successful payment");
+                }
+                else
+                    Response.Write(response.ResponseStatus);
+            }
+        }
+    }
+
+    public class TransactionResult
+    {
+        public int ErrorCode { get; set; }
+        public string ErrorText { get; set; }
+        public decimal Amount { get; set; }
+        public Guid TransactionId { get; set; }
+    }
+}
