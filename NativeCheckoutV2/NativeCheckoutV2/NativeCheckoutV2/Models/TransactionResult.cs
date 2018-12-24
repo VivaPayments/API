d5f9591ff3ad1d2c6c1343829f447001c63cf413
Dimitris Pnevmatikos <dpnevmatikos@vivawallet.com>
2018-12-24 12:37:27 +0200

    Add ASPNET Core sample project for Native Checkout v2

---
+++ NativeCheckoutV2/NativeCheckoutV2/NativeCheckoutV2/Models/TransactionResult.cs
@@ -0,0 +1,14 @@
+﻿namespace NativeCheckoutV2.Models
+{
+    using System;
+
+    public class TransactionResult
+    {
+        public int ErrorCode { get; set; }
+        public string StatusId { get; set; }
+        public string ErrorText { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public Guid? TransactionId { get; set; }
+        public string CorrelationId { get; set; }
+    }
+}
