71451d01898f6ff85daae7979b06ad7503930aa7
Tom van der Veer <tom@hellasboost.gr>
2019-01-11 19:24:07 +0200

    Magento 2 Currency validation bug

--- Plugins/magento/magento2_vivawallet/Ced/VivaPayments/Model/PaymentMethod.php
+++ Plugins/magento/magento2_vivawallet/Ced/VivaPayments/Model/PaymentMethod.php
@@ -255,6 +255,16 @@ class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
         return $this->_getUrl('checkout/onepage/failure', $storeId);
     }
 
+	public function getCheckout()
+    {
+        return $this->_checkoutSession;
+    }
+	
+	public function getQuote()
+    {
+        return $this->getCheckout()->getQuote();
+    }
+
     protected function _getUrl($path, $storeId, $secure = null)
     {
         $store = $this->_storeManager->getStore($storeId);
@@ -278,7 +288,7 @@ class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
             throw new \Magento\Framework\Validator\Exception(__('Selected payment type is not allowed for billing country.'));
         }
 		
-		$currency_code = $data->getQuote()->getBaseCurrencyCode();
+		$currency_code = $this->getQuote()->getBaseCurrencyCode();
         if (isset($currency_code) && $currency_code!='' && !in_array($currency_code, $this->getsupportedCurrencyCodes())) {
             throw new \Magento\Framework\Exception\LocalizedException(
                 __('Selected currency code ('.$currency_code.') is not compatible.')
