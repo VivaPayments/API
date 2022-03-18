25ba4a571707d2a64f5b978178e85c797677b27b
can3ry <51880555+can3ry@users.noreply.github.com>
2022-03-18 01:10:33 +0000

    Update gettransactions.php

--- CreateOrder/PHP/gettransactions.php
+++ CreateOrder/PHP/gettransactions.php
@@ -1,60 +1,51 @@
-<?php 
-header('content-type: text/html;charset=utf8');
+<?php
 
-// The POST URL and parameters
-$request =  'https://demo.vivapayments.com/api/transactions/';	// demo environment URL
-//$request =  'https://www.vivapayments.com/api/transactions';	// production environment URL
+// Bearer must be generated with OAuth 2 and scope: urn:viva:payments:core:api:redirectcheckout
+// https://developer.vivawallet.com/tutorials-for-payments/enable-oauth2-authentication/
+$accessToken   = 'xxx';
+$transactionId = 'xxx';
 
-// Your merchant ID and API Key can be found in the 'Security' settings on your profile.
-$merchant_id = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
-$api_key = 'xxxxxxxxxxxxx';
+$curl = curl_init();
 
-// Set your order code here
-$order_code = xxxxxxxxxxxxxxxx; // int
+curl_setopt_array($curl, array(
+    CURLOPT_URL            => "https://demo-api.vivapayments.com/checkout/v2/transactions/$transactionId",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING       => '',
+    CURLOPT_MAXREDIRS      => 10,
+    CURLOPT_TIMEOUT        => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST  => 'GET',
+    CURLOPT_HTTPHEADER     => array(
+        "Authorization: Bearer $accessToken"
+    ),
+));
 
-$getargs = '?ordercode='.urlencode($order_code);
+$response = curl_exec($curl);
 
-// Get the curl session object
-$session = curl_init($request);
+curl_close($curl);
+echo $response;
 
-// Set the GET options.
-curl_setopt($session, CURLOPT_HTTPGET, true);
-curl_setopt($session, CURLOPT_URL, $request . $getargs);
-curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($session, CURLOPT_USERPWD, $merchant_id.':'.$api_key);
-curl_setopt($session, CURLOPT_SSL_CIPHER_LIST, 'TLSv1.2');
-
-// Do the GET and then close the session
-$response = curl_exec($session);
-
-curl_close($session);
-
-// Parse the JSON response
-try {
-	// echo $response . '<br /><br />'; // you can see all properties with their values in a json string here.
-		
-	if(is_object(json_decode($response))){
-	  	$resultObj=json_decode($response);
-	}
-} catch( Exception $e ) {
-	echo $e->getMessage();
+/*
+{
+    "email": "someone@vivawallet.com",
+    "amount": 1,
+    "orderCode": 6810910170372602,
+    "statusId": "F",
+    "fullName": "George Seferis",
+    "insDate": "2021-12-13T11:16:32.541Z",
+    "cardNumber": "523929XXXXXX0168",
+    "currencyCode": 978,
+    "customerTrns": "This is a description displayed to the customer",
+    "merchantTrns": "This is a short description that helps you uniquely identify the transaction",
+    "transactionTypeId": 5,
+    "recurringSupport": true,
+    "totalInstallments": 0,
+    "cardCountryCode": null,
+    "cardIssuingBank": null,
+    "currentInstallment": 0,
+    "cardTypeId": 1
 }
-if ($resultObj->ErrorCode==0){	//success when ErrorCode = 0
-	if(sizeof($resultObj->Transactions) > 0) {
-		foreach ($resultObj->Transactions as $t){ // an order might have more than one transactions, or no transactions yet.
-			echo 'TransactionId: ' . $t->TransactionId . '<br />';
-			echo 'Amount: ' . $t->Amount . '<br />';
-			echo 'StatusId: ' . $t->StatusId . '<br />';
-			// check https://developer.vivawallet.com/web-api-integration/transaction-parameters/ for status result ids
-			// "F" = The transaction has been completed successfully
-			// "E" = The transaction was not completed because of an error
-			echo 'CustomerTrns: ' . $t->CustomerTrns . '<br />';
-		}
-	} else {
-		echo 'No transactions found. Make sure the order code exists and is created by your account.';
-	}
-} else {
-	echo 'The following error occured: <strong>' . $resultObj->ErrorCode . '</strong>, ' . $resultObj->ErrorText;
-}
+*/
 
 ?>
